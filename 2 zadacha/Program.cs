bf3ab38b7c5fac57fd4ebe6c05da72051238f408
Yana <yadivenko@gmail.com>
2023-01-16 23:53:18 +0300

    2 zadacha

---
+++ 2 zadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32 (Console.ReadLine ());
+
+if (a > b && a > c)
+{
+     Console.WriteLine("{0} max ", a);
+}
+
+else if (b > a && a > c)
+{
+    Console.WriteLine("{0} max ", b);
+}
+
+else if (c > a && c > b)
+{
+    Console.WriteLine("{0} max ", c);
+}
+
