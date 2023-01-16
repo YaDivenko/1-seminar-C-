113647f274911ca58dc794d40153e876745328e2
Yana <yadivenko@gmail.com>
2023-01-16 23:49:13 +0300

    1 zadacha

---
+++ 1 zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine ());
+
+if (a > b)
+{
+     Console.WriteLine("{0} max ", a);
+}
+else 
+{
+    Console.WriteLine("{0} max ", b);
+}
